26afa55d349c5da096c0f2ff70162c3947017f78
Plin <zhi.chen@unity3d.com>
2020-09-02 13:37:32 +0800

    modify build_from_unity.sh

--- wechat-project/build_from_unity.sh
+++ wechat-project/build_from_unity.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-cp ../unity_empty_h5/wasm_pub_empty_h5/Build/wasm_pub_empty_h5.data.unityweb ./wasm_pub_empty_h5.data.unityweb.bin
-cp -rf ../unity_empty_h5/wasm_pub_empty_h5/Build/wasm_pub_empty_h5.wasm.code.unityweb ./wasm_pub_empty_h5.wasm.code.unityweb.bin
-brotli ../unity_empty_h5/wasm_pub_empty_h5/Build/wasm_pub_empty_h5.wasm.code.unityweb -f
-cp ../unity_empty_h5/wasm_pub_empty_h5/Build/wasm_pub_empty_h5.wasm.code.unityweb.br ./wasm_pub_empty_h5.wasm.br.bin
-cp ../unity_empty_h5/wasm_pub_empty_h5/Build/wasm_pub_empty_h5.wasm.framework.unityweb ./wasm_pub_empty_h5.wasm.framework.unityweb.js
-sed -i '1i\exports.start=' ./wasm_pub_empty_h5.wasm.framework.unityweb.js
+game_name=wasm_pub_empty_h5
+cp ${game_name}.data.unityweb ./${game_name}.data.unityweb.bin
+cp -rf ${game_name}.wasm.code.unityweb ./${game_name}.wasm.code.unityweb.bin
+brotli ${game_name}.wasm.code.unityweb -f
+cp ${game_name}.wasm.code.unityweb.br ./${game_name}.wasm.br.bin
+cp ${game_name}.wasm.framework.unityweb ./${game_name}.wasm.framework.unityweb.js
+sed -i '1i\exports.start=' ./${game_name}.wasm.framework.unityweb.js
 echo "build succeeded!"
+
