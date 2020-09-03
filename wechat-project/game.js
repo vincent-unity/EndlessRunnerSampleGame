b58d323dfb0690e71ae081af667ee9ae75b42297
Plin <zhi.chen@unity3d.com>
2020-09-03 14:31:48 +0800

    clean

--- wechat-project/game.js
+++ wechat-project/game.js
@@ -6,8 +6,9 @@ canvas.style.height = window.innerHeight * window.devicePixelRatio //获取屏�
 canvas.width =  window.innerWidth  * window.devicePixelRatio //真实的像素宽度
 canvas.height = window.innerHeight * window.devicePixelRatio //真实的像素高度
 console.log('innerWidth', window.innerWidth, window.innerHeight, window.devicePixelRatio)
- 
- console.log("wx.getSystemInfoSync : \n" + JSON.stringify(wx.getSystemInfoSync()))
+
+var gameName = "wasm_pub_empty_h5";
+
 GameGlobal.Module = {};
 GameGlobal.UnityLoader = {
 SystemInfo: {
@@ -48,8 +49,8 @@ var gameInstance = {
   compatibilityCheck: undefined,
   Module: {
     IsWxGame: true,
-    preLoaDataPath: 'wasm_pub_empty_h5.data.unityweb.bin',//.bin
-    wasmPath: 'wasm_pub_empty_h5.wasm.br.bin',//wasm_pub_empty_h5.wasm.br.bin // wasm_pub_empty_h5.wasm.code.unityweb.bin
+    preLoaDataPath: gameName + '.data.unityweb.bin',//.bin
+    wasmPath: gameName + '.wasm.code.unityweb.br.bin',//.wasm.br.bin // .wasm.code.unityweb.bin
     // wasmBin:"",
     graphicsAPI: ["WebGL 2.0", "WebGL 1.0"],
     onAbort: function(what){
@@ -88,57 +89,34 @@ var gameInstance = {
   },
 };
 
-GameGlobal.cdn = "http://10.86.98.91:8080/";
+GameGlobal.alert = function(msg){wx.showToast({title: msg})}
+
+GameGlobal.cdn = "http://10.86.98.91:8080/";// !!!ATTENTION!! please end with "/"
 
 function startUnity(){
   gameInstance.Module.gameInstance = gameInstance;
   gameInstance.popup = function (message, callbacks) { return UnityLoader.Error.popup(gameInstance, message, callbacks); };
 
-  GameGlobal.Module = gameInstance.Module
-  var framework = require('./wasm_pub_empty_h5.wasm.framework.unityweb.js');
-  // const { Module } = require('module');
-  framework.start(GameGlobal.Module)
+  GameGlobal.Module = gameInstance.Module 
+  require('./' + gameName + '.wasm.framework.unityweb.js')
+  GameGlobal.UnityModule(GameGlobal.Module)
   var gl = canvas.getContext("webgl");
   gl.scissor(0, 0, canvas.width, canvas.height);
 }
 
 var platform = wx.getSystemInfoSync().platform;
 if(platform == "devtools") {
-  gameInstance.Module["wasmPath"] = 'wasm_pub_empty_h5.wasm.code.unityweb.bin';
-  // gameInstance.Module["preLoaDataPath"] = 'wasm_pub_empty_h5.data.unityweb.bin';
-  // // gameInstance.Module["wasmBin"]=wx.getFileSystemManager().readFileSync(gameInstance.Module["wasmPath"]);
-  // // gameInstance.Module["rawData"]=wx.getFileSystemManager().readFileSync(gameInstance.Module["preLoaDataPath"]);
+  gameInstance.Module["wasmPath"] = gameName + '.wasm.code.unityweb.bin';
+  gameInstance.Module["preLoaDataPath"] = gameName + '.data.unityweb.bin';
   startUnity();
 } else {
   var dataLoaded=0, codeLoaded=0;
-
-  // wx.request({
-  //   url: cdn + gameInstance.Module["wasmPath"],
-  //   responseType: 'arraybuffer',
-  //   timeout:10000,
-  //   success: ({ data }) => {
-  //     console.log("data : \n" + data)
-  //     codeLoaded =1;
-  //     gameInstance.Module["wasmBin"] = data;//decompress(data);
-  //     // gameInstance.Module["wasmBin"] = GameGlobal.UnityLoader.Compression.brotli.decompress(data);
-  //     console.log("wasm bin loaded  ");
-  //     if(dataLoaded){
-  //       startUnity();
-  //     }
-  //   },
-  //   fail:function(res){
-  //     console.log("xxxxxxxxxxxxxxxxxxxxxxxxxx");
-  //     console.log("res.errorMsg: " + res.errMsg);
-  //     console.log("xxxxxxxxxxxxxxxxxxxxxxxxxx");
-  //   }
-  // });
-  
   wx.request({
     url: cdn + gameInstance.Module["preLoaDataPath"],
     responseType: 'arraybuffer',
     timeout: 10000,
     success: ({ data }) => {
-      dataLoaded =1;
+      dataLoaded = 1;
       gameInstance.Module["rawData"] = data;
       console.log("raw Data loaded  ");
       if(codeLoaded){
@@ -147,29 +125,13 @@ if(platform == "devtools") {
     }
   });
   
-  // wx.downloadFile({
-  //   url: cdn + gameInstance.Module["preLoaDataPath"],
-  //   success:(res)=>{
-  //     if(res.statusCode == 200){
-  //       var path = wx.getFileSystemManager().saveFileSync(res.tempFilePath, wx.env.USER_DATA_PATH+"/"+gameInstance.Module["preLoaDataPath"]);
-  //       gameInstance.Module["preLoaDataPath"] = path;
-  //       dataLoaded =1;
-  //       console.log("dataLoaded:  " + path);
-  //       // if(codeLoaded){
-  //         startUnity();
-  //       // }
-  //     }
-  //   }
-  // });
-  
   wx.downloadFile({
     url: cdn + gameInstance.Module["wasmPath"],
     success:(res)=>{
       if(res.statusCode == 200){
-  
         var path = wx.getFileSystemManager().saveFileSync(res.tempFilePath, wx.env.USER_DATA_PATH+"/"+gameInstance.Module["wasmPath"]);
         gameInstance.Module["wasmPath"] = path;
-        codeLoaded =1;
+        codeLoaded = 1;
         console.log("codeLoaded:  " + path);
         if(dataLoaded){
           startUnity();
